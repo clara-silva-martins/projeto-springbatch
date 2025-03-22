5cd1cd5bb783854c604c6075ab95d6bce7f322f9
clara-silva-martins <clara.dsm@gmail.com>
2025-03-22 16:07:59 -0300

    Começando os estudos de spring batch

---
+++ src/main/java/br/com/tickets/codetickets/Importacao.java
@@ -0,0 +1,96 @@
+package br.com.tickets.codetickets;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+public class Importacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cpf;
+    private String cliente;
+    private LocalDate dataNascimento;
+    private String evento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEvento() {
+        return evento;
+    }
+
+    public void setEvento(String evento) {
+        this.evento = evento;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getTipoIngresso() {
+        return tipoIngresso;
+    }
+
+    public void setTipoIngresso(String tipoIngresso) {
+        this.tipoIngresso = tipoIngresso;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDateTime getHoraImportacao() {
+        return horaImportacao;
+    }
+
+    public void setHoraImportacao(LocalDateTime horaImportacao) {
+        this.horaImportacao = horaImportacao;
+    }
+
+    private LocalDate data;
+    private String tipoIngresso;
+    private Double valor;
+    private LocalDateTime horaImportacao;
+}
+
